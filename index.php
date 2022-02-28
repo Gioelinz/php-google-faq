fc729e4e1e621611b3cf9e11ce96fb30c2ac494d
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-28 14:54:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+/* Riscrivere questa pagina del sito google:
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Realizziamo la struttura dati che ci sembra più adatta per poter poi stampare con un ciclo le domande e risposte della sezione centrale. */
